538eac2648eddbac5ce95512afb0088ee5a54b1b
Max <savagecm@users.noreply.github.com>
2017-09-25 16:40:05 +0800

    first commit

--- example/example.cpp
+++ example/example.cpp
@@ -1,5 +1,6 @@
 
 #include "logger/logger.hpp"
+#include "uri.h"
 
 int main()
 {
@@ -12,4 +13,8 @@ int main()
                     << "this is info log");
     __LOG(debug, "hello logger!"
                      << "this is debug log");
+    web::uri u(U("http://testname.com:81/path?baz"));
+    web::uri a = u.authority();
+    __LOG(error, "\npath is : " << u.path() << "\n scheme() is : " << a.scheme() << "\n host() is : " << a.host() << "\n port is : " << a.port());
+    __LOG(error, "to string : " << a.to_string());
 }
